9e305f85a28cb0ffe1643182ed5b9918f99593e3
minchodang <minsu910725@gmail.com>
2024-01-26 21:43:11 +0900

    Fix: 잘못된 함수  선언 수정

--- src/pages/home/components/tests/ProductFilter.spec.jsx
+++ src/pages/home/components/tests/ProductFilter.spec.jsx
@@ -12,14 +12,15 @@ const setTitleFn = vi.fn();
 
 beforeEach(() => {
   mockUseFilterStore({
-    setMaxPriceFn: setMaxPriceFn,
-    setMinPriceFn: setMinPriceFn,
+    setMinPrice: setMinPriceFn,
+    setMaxPrice: setMaxPriceFn,
     setTitle: setTitleFn,
   });
 });
 
 it('카테고리 목록을 가져온 후 카테고리 필드의 정보들이 올바르게 렌더링된다.', async () => {
   await render(<ProductFilter />);
+
   expect(await screen.findByLabelText('category1')).toBeInTheDocument();
   expect(await screen.findByLabelText('category2')).toBeInTheDocument();
   expect(await screen.findByLabelText('category3')).toBeInTheDocument();
@@ -27,6 +28,7 @@ it('카테고리 목록을 가져온 후 카테고리 필드의 정보들이 올
 
 it('상품명을 수정하는 경우 setTitle 액션이 호출된다.', async () => {
   const { user } = await render(<ProductFilter />);
+
   const textInput = screen.getByLabelText('상품명');
   await user.type(textInput, 'test');
 
@@ -35,8 +37,10 @@ it('상품명을 수정하는 경우 setTitle 액션이 호출된다.', async ()
 
 it('카테고리를 클릭 할 경우의 클릭한 카테고리가 체크된다.', async () => {
   const { user } = await render(<ProductFilter />);
+
   const category3 = await screen.findByLabelText('category3');
   await user.click(category3);
+
   expect(category3).toBeChecked();
 });
 
@@ -45,9 +49,11 @@ it('최소 가격 또는 최대 가격을 수정하면 setMinPrice과 setMaxPric
 
   const minPriceTextInput = screen.getByPlaceholderText('최소 금액');
   await user.type(minPriceTextInput, '1');
+
   expect(setMinPriceFn).toHaveBeenCalledWith('1');
 
   const maxPriceTextInput = screen.getByPlaceholderText('최대 금액');
   await user.type(maxPriceTextInput, '2');
+
   expect(setMaxPriceFn).toHaveBeenCalledWith('2');
 });
