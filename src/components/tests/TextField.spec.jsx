d2e53d07a63d6f6a441e8a31eaabe0377302c2ba
minchodang <minsu910725@gmail.com>
2024-01-30 22:02:02 +0900

    Feat: tdd-example

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,39 +4,16 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('className prop으로 설정한 css class가 적용된다.', async () => {
-  //Arrange = 테스트를 위한 환경 만들기.
-  // -> className을 지닌 컴포넌트 렌더링.
-  //Act - 테스트할 동작 발생.
-  // -> 렌더링에 대한 검증이기 때문에 이 단계는 생략
-  // -> 클릭이나 메서드 호출, prop 변경 등등에 대한 작업이 여기에 해당
-  // Assert - 올바른 동작이 실행되었는 지 검증
-  // -> 렌더링 후 DOM에 해당 class가 존재하는지 검증
-
-  // render API를 호출 -> 테스트 환경의 jsDOM에 리액트 컴포넌트가 렌더링 된 DOM 구조가 반영
-  // jsDOM: Node.js에서 사용하기 위해 많은 웹 표준을 순수 자바스크립트로 구현.
-  await render(<TextField className="my-class" />);
-
+it('기본 placeholder"텍스트를 입력해 주세요."가 노출된다.', async () => {
+  await render(<TextField />);
   const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
-
-  screen.debug();
-
-  //vitest의 expect 함수를 사용하여 기대 결과를 검증.\
-
-  // className이란 내부 props이나 state 값을 검증 (X)
-  // 렌더링되는 DOM 구조가 올바르게 변경되었는지 확인 (O) -> 최종적으로 사용자가 보는 결과는 DOM
-  expect(textInput).toHaveClass('my-class');
+  expect(textInput).toBeInTheDocument();
 });
 
-describe('placeholder', () => {
-  it('기본 placeholder"텍스트를 입력해 주세요."가 노출된다.', async () => {
-    await render(<TextField />);
-    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
-    expect(textInput).toBeInTheDocument();
-  });
-  it('placeholder prop에 따라 placeholder가 변경된다.', async () => {
-    await render(<TextField placeholder="상품명을 입력해 주세요." />);
-    const textInput = screen.getByPlaceholderText('상품명을 입력해 주세요.');
-    expect(textInput).toBeInTheDocument();
-  });
+it('텍스트를 입력하면 onChange prop으로 등록한 함수가 호출된다.', async () => {
+  const spy = vi.fn();
+  const { user } = await render(<TextField onChange={spy} />);
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+  await user.type(textInput, 'test');
+  expect(spy).toHaveBeenCalledWith('test');
 });
