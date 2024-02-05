8795c03e46c9f0ca5cb3c66000b75ff1e845c3c6
minchodang <minsu910725@gmail.com>
2024-02-06 00:12:28 +0900

    Feat: cypress-query

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,15 +1,16 @@
 beforeEach(() => {
   cy.visit('/login');
+  cy.findByLabelText('로그인').as('loginBtn');
 });
 
 it('이메일을 입력하지 않고 로그인 버튼을 클릭할 경우 "이메일을 입력하세요" 경고 메세지가 노출된다', () => {
   cy.findByLabelText('로그인').click();
-  cy.findByText('이메일을 입력하세요').should('exist');
+  cy.get('@loginBtn').should('exist');
 });
 
 it('비밀번호를 입력하지 않고 로그인 버튼을 클릭할 경우 "비밀번호를 입력하세요" 경고 메세지가 노출된다', () => {
   cy.findByLabelText('로그인').click();
-  cy.findByText('비밀번호를 입력하세요').should('exist');
+  cy.get('@loginBtn').should('exist');
 });
 
 it('잘못된 양식의 이메일을 입력한 뒤 로그인 버튼을 클릭할 경우 "이메일 양식이 올바르지 않습니다" 경고 메세지가 노출된다', () => {
